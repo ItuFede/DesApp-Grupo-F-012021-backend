c9ffc70cb8f05103439b11db1e6da8ab1e5f7057
ItuFede <federicoituarte1@gmail.com>
2021-05-02 23:23:27 -0300

    add jacoco coverage

--- src/main/java/ar/edu/unq/desapp/grupof012021/backenddesappapi/service/MediaService.java
+++ src/main/java/ar/edu/unq/desapp/grupof012021/backenddesappapi/service/MediaService.java
@@ -1,10 +1,7 @@
 package ar.edu.unq.desapp.grupof012021.backenddesappapi.service;
 
 import ar.edu.unq.desapp.grupof012021.backenddesappapi.model.entity.Review;
-import ar.edu.unq.desapp.grupof012021.backenddesappapi.persistence.MediaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 public interface MediaService {
-    public void addReviewTo(long id, Review aReview);
+    void addReviewTo(long id, Review aReview);
 }
