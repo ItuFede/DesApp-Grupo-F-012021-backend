d3e246670275985addebbff08ad6fce631630783
ItuFede <federicoituarte1@gmail.com>
2021-07-03 20:06:58 -0300

    add serializer and change time on Redis

--- src/main/java/ar/edu/unq/desapp/grupof012021/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupof012021/backenddesappapi/BackendDesappApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -19,7 +21,8 @@ public class BackendDesappApiApplication {
 	@Bean
 	public RedisCacheConfiguration cacheConfiguration() {
 		return RedisCacheConfiguration.defaultCacheConfig()
-				.entryTtl(Duration.ofMinutes(5))
-				.disableCachingNullValues();
+				.entryTtl(Duration.ofMinutes(15))
+				.disableCachingNullValues()
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 	}
 }
